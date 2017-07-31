c7b1eb433c5c098983d21160299bd048eb5529af
Derek Bronston <dbronston@me.com>
2017-07-31 10:45:36 -0600

    Updated Unit tests

--- Tests/BDTestsUnitTests/BDTestsUnitTestsUITests/BDTestsUnitTestsUITests.swift
+++ Tests/BDTestsUnitTests/BDTestsUnitTestsUITests/BDTestsUnitTestsUITests.swift
@@ -15,15 +15,7 @@ class BDTestsUnitTestsUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        //XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -49,7 +41,6 @@ class BDTestsUnitTestsUITests: XCTestCase {
         
          app.buttons["One"].tap()
         //app.buttons["Submit"].tap()
-        
         
     }
     
