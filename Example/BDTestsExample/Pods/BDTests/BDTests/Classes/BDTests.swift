d2dea011e8c2c3d89b1e23fbbb8a0c773e5f9c05
Andrei Ahibalau <andrei.ahibalau@freshly.com>
2021-03-01 17:02:19 +0300

    Update example project

--- Example/BDTestsExample/Pods/BDTests/BDTests/Classes/BDTests.swift
+++ Example/BDTestsExample/Pods/BDTests/BDTests/Classes/BDTests.swift
@@ -10,13 +10,34 @@ import UIKit
 import OHHTTPStubs
 
 
+/*
+ Custom pustboard is used, because tests with UIPasteboard has ~x4 execution time
+ e.g.
+ Custom Pasteboard: Executed 2351 tests, with 0 failures (0 unexpected) in 225.449 (227.031) seconds
+ UIPasteboard:      Executed 2350 tests, with 0 failures (0 unexpected) in 808.095 (809.200) seconds
+ */
+private class Pasteboard {
+    static var general = Pasteboard()
+
+    func setItems(_ items: [[String : Any]]) {
+        self.items = items
+    }
+
+    var items: [[String : Any]] = []
+
+    func addItems(_ items: [[String : Any]]) {
+        self.items = self.items + items
+    }
+}
+
+
 public class BDTests  {
     
     //DEFAULT ENVIORNMENT NAME, USED TO DETERMINE IF WE ARE IN TEST MODE. CHANGE FTO YOUR ENVIORMENT
-     public var enviornmentName = "BD-UI-TEST"
+    public var enviornmentName = "BD-UI-TEST"
     
     //DEFAULT HTTP RESPONSE CODE TO 200
-     public var httpResponseCode:Int32 = 200
+    public var httpResponseCode:Int32 = 200
     
     public init(enviornmentName:String?){
         if let envName = enviornmentName {
@@ -24,11 +45,21 @@ public class BDTests  {
         }
     }
     
-    /*
-     CREATE TEST
+    
+    /**
+     Creates a test
+     
+     - parameter jsonString: String?
+     - parameter jsonFile:String?
+     - parameter httpCode:Int32
+     
+     - return: Bool
      */
-    public func createTest(jsonString:String?,jsonFile:String?,httpCode:Int32)->Bool{
-        print("CREATE TEST")
+    public func createTest(jsonString: String?, jsonFile: String?, httpCode: Int32) -> Bool {
+        
+        //CLEAR
+        //Pasteboard.general.setValue(nil, forKey: "data")
+        
         var created = false
         
         //SET THE HTTP RESPONSE CODE
@@ -53,131 +84,185 @@ public class BDTests  {
         return created
     }
     
-    /*
-     seed database
+    /**
+     ==== BASE ====
+     The ref variable is the string representation on the method that we want to call in order to set up test
+     ==== BASE ====
+     
+     - parameter ref: String
+     
+     - return: Bool
      */
-    public func seedDatabase(json:String)->Bool{
+    public func seedDatabase(ref: String) -> Bool {
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName+"-model"), create: true)
-        paste?.string = ""
-        paste?.string = json
+        //CLEAR
+        removeModelTest()
+        let currentItems = Pasteboard.general.items
+        var newItems = [[String: Any]]()
+        for item in currentItems {
+            if item["model"] == nil {
+                newItems.append(item)
+            }
+        }
+        newItems.append(["model": ref])
+        Pasteboard.general.addItems(newItems)
         
-        if paste == nil { return false }
         return true
     }
     
     /**
-     read database data
+     Reads the ref variable out of the clipboard in order to set up the test
+     
+     - retrun: String?
      */
-    public func readDatabaseData()->String?{
+    public func readDatabaseData() -> String? {
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName+"-model"), create: true)
-        if paste == nil { return nil }
-        
-        //guard let json = paste?.string else { return nil }
-        //guard let dict = self.convertToDictionary(text: json) else { return nil }
-        
-        //CLEAR CLIPBOARD
-       //UIPasteboard.remove(withName: UIPasteboardName(rawValue: self.enviornmentName+"-model"))
-        
-        return paste?.string
-    }
-    
-    
-    
-    public func removeTest(){
-        self.removeStubs()
-        UIPasteboard.remove(withName: UIPasteboardName(rawValue: self.enviornmentName))
-        UIPasteboard.remove(withName: UIPasteboardName(rawValue: self.enviornmentName+"-model"))
-    }
-    
-    public func removeModelTest(){
-        self.removeStubs()
-        UIPasteboard.remove(withName: UIPasteboardName(rawValue: self.enviornmentName+"-model"))
-    }
-    
-    /*
-     READ DATA FILE INTO STRING
-     */
-    public func openFileAndReadIntoString(urlString:String)->String?{
-        if let dir = Bundle.main.path(forResource: urlString, ofType:"json"){
-            do {
-                let text2 = try String(contentsOfFile: dir)
-                return text2
-            }catch _ as NSError{
-                return nil
+        let items =  Pasteboard.general.items
+        for item in items {
+            if let model = item["model"]  {
+                guard let modelData = model as? Data else { return nil }
+                guard let methodName = String(data: modelData, encoding: .utf8) else { return nil }
+                return methodName
             }
         }
         return nil
     }
     
-    /*
+    
+    /**
+     Removes all tests. Removes:stubs, pasteboards
+     
+     - return: none
+     */
+    public func removeTest() {
+        
+        let currentItems = Pasteboard.general.items
+        var newItems = [[String: Any]]()
+        for item in currentItems{
+            if item["data"] == nil {
+                newItems.append(item)
+            }
+        }
+        Pasteboard.general.items = newItems
+    }
+    
+    
+    /**
+     removes a model test and stubs
+     
+     */
+    public func removeModelTest() {
+        
+        let currentItems = Pasteboard.general.items
+        var newItems = [[String:Any]]()
+        for item in currentItems{
+            if item["model"] == nil {
+                newItems.append(item)
+            }
+        }
+        Pasteboard.general.items = newItems
+    }
+    
+    /**
+     READ DATA FILE INTO STRING
+     
+     - parameter: urlString:String
+     - return: String?
+     */
+    public func openFileAndReadIntoString(urlString: String) -> String? {
+        guard let dir = Bundle.main.path(forResource: urlString, ofType: "json") else { return nil }
+        do {
+            return try String(contentsOfFile: dir)
+        } catch _ as NSError {
+            return nil
+        }
+    }
+    
+    /**
      READ CLIPBOARD
      
      1. http code
      2. json string?
      3. json file
      
+     - return: String?
      */
-    public func readClipboard()->String?{
+    public func readClipboard() -> String? {
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName), create: true)
+        let items = Pasteboard.general.items
+        for item in items {
+            if let model = item["data"]  {
+                guard let modelData = model as? Data else { return nil }
+                guard let methodName = String(data: modelData, encoding: .utf8) else { return nil }
+                return methodName
+            }
+        }
         
-        if paste == nil { return nil }
-        let clipString =  paste?.string
-        
-        //clean clipboard
-        UIPasteboard.remove(withName: UIPasteboardName(rawValue: self.enviornmentName))
-        
-        return clipString
-        
+        return nil
     }
     
-    /*
+    /**
      SET CLIPBOARD
      pass json into clipboard for later review
-     
      
      1. http code
      2. json string?
      3. json file
+     
+     - parameter json:String
+     - return: Bool
+     
      */
-    public func setClipboard(json:String)->Bool{
+    public func setClipboard(json: String) -> Bool {
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName), create: true)
-        paste?.string = ""
-        paste?.string = json
+        //CLEAR
+        removeTest()
+        let currentItems = Pasteboard.general.items
+        var newItems = [[String: Any]]()
+        for item in currentItems{
+            if item["data"] == nil {
+                newItems.append(item)
+            }
+        }
         
-        if paste == nil { return false }
+        newItems.append(["data":json])
+        Pasteboard.general.addItems(newItems)
         return true
     }
     
-    /*
+    /**
      CONVERT JSON TO DICTIONARY
+     
+     - parameter text:String
+     - return: [String:Any]?
      */
     public func convertToDictionary(text: String) -> [String: Any]? {
-        if let data = text.data(using: .utf8) {
-            do {
-                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-            } catch {
-                print(error.localizedDescription)
-            }
+        guard let data = text.data(using: .utf8) else { return nil }
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+            return nil
         }
-        return nil
     }
     
     
-    /*
-     DETERMINE RESOPNSE TEXT
+    /**
+     DETERMINE RESPONSE TEXT
+     
+     - parameter dict[String:Any]
+     - return: String?
      */
-    public func determineResponseText(dict:[String:Any])->String?{
+    public func determineResponseText(dict: [String: Any]) -> String? {
         
         if dict["data"] != nil {
             do {
                 let jsonData = try JSONSerialization.data(withJSONObject: dict["data"]!, options: .prettyPrinted)
                 // here "jsonData" is the dictionary encoded in JSON data
-                let response = String.init(data: jsonData, encoding: .utf8)
-                guard let responseText = response else { assert(false); return nil}
+                guard let responseText = String(data: jsonData, encoding: .utf8) else {
+                    assert(false)
+                    return nil
+                }
                 return responseText
             }catch _ as NSError{
                 //assert(false);
@@ -185,9 +270,11 @@ public class BDTests  {
             }
         }
         
-        if dict["data-file"] as? String != nil {
-            
-            guard let responseText = self.openFileAndReadIntoString(urlString: dict["data-file"]! as! String) else { assert(false); return nil }
+        if let dataFileUrl = dict["data-file"] as? String {
+            guard let responseText = self.openFileAndReadIntoString(urlString: dataFileUrl) else {
+                assert(false)
+                return nil
+            }
             
             return responseText
         }
@@ -195,75 +282,143 @@ public class BDTests  {
         return nil
     }
     
-    /*
+    /**
      STUB NETWORK
      */
-    public func runTests()->Bool{
+    public func runTests() -> Bool {
         
-        guard let json = self.readClipboard() else { assert(false); return false }
-        guard let dict = self.convertToDictionary(text: json) else { assert(false); return false }
-        guard let responseText = self.determineResponseText(dict:dict) else { assert(false); return false }
-        guard let httpCode = dict["code"] else { assert(false); return false }
-        guard let code =  httpCode as? Int32 else { assert(false); return false }
-        
-        stub(condition: isMethodGET()) { request -> OHHTTPStubsResponse in
+        stub(condition: isMethodGET()) { request -> HTTPStubsResponse in
+            let data = self.readData()
+            guard data.code > 0 else {
+                return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+            }
+            let code = data.code
+            let responseText = data.response
+            //print(responseText)
             let stubData = responseText.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+            return HTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
         }
         
-        stub(condition: isMethodPOST()) { request -> OHHTTPStubsResponse in
-            let stubData = responseText.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+        stub(condition: isMethodPOST()) { request -> HTTPStubsResponse in
+            let data = self.readData()
+            if data.code > 0 {
+                let code = data.code
+                let responseText = data.response
+                //print(responseText)
+                let stubData = responseText.data(using: String.Encoding.utf8)
+                return HTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+            }else {
+                return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+            }
         }
         
-        stub(condition: isMethodPUT()) { request -> OHHTTPStubsResponse in
-            let stubData = responseText.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+        stub(condition: isMethodPUT()) { request -> HTTPStubsResponse in
+            let data = self.readData()
+            if data.code > 0 {
+                let code = data.code
+                let responseText = data.response
+                //print(responseText)
+                let stubData = responseText.data(using: String.Encoding.utf8)
+                return HTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+            }else {
+                return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+            }
         }
         
-        stub(condition: isMethodPATCH()) { request -> OHHTTPStubsResponse in
-            let stubData = responseText.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+        stub(condition: isMethodPATCH()) { request -> HTTPStubsResponse in
+            let data = self.readData()
+            if data.code > 0 {
+                let code = data.code
+                let responseText = data.response
+                // print(responseText)
+                let stubData = responseText.data(using: String.Encoding.utf8)
+                return HTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+            }else {
+                return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+            }
         }
         
-        stub(condition: isMethodDELETE()) { request -> OHHTTPStubsResponse in
-            let stubData = responseText.data(using: String.Encoding.utf8)
-            return OHHTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+        stub(condition: isMethodDELETE()) { request -> HTTPStubsResponse in
+            let data = self.readData()
+            if data.code > 0 {
+                let code = data.code
+                let responseText = data.response
+                //print(responseText)
+                let stubData = responseText.data(using: String.Encoding.utf8)
+                return HTTPStubsResponse(data:stubData!, statusCode:code, headers:nil)
+            }else {
+                return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+            }
         }
         return true
     }
     
-    /**
-     REMOVE STUBS
-     */
-    public func removeStubs(){
-        OHHTTPStubs.removeAllStubs()
+    public func runTestsAs200()->Bool{
+        
+        stub(condition: isMethodGET()) { request -> HTTPStubsResponse in
+            return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+        }
+        
+        stub(condition: isMethodPOST()) { request -> HTTPStubsResponse in
+            return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+        }
+        
+        stub(condition: isMethodPUT()) { request -> HTTPStubsResponse in
+            return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+        }
+        
+        stub(condition: isMethodPATCH()) { request -> HTTPStubsResponse in
+            return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+        }
+        
+        stub(condition: isMethodDELETE()) { request -> HTTPStubsResponse in
+            return HTTPStubsResponse(data:Data(), statusCode:400, headers:nil)
+        }
+        return true
     }
     
-    /*
+    func readData()->(code:Int32,response:String){
+        
+        let resp = (code:Int32(0),response:"")
+        guard let json = self.readClipboard() else { return resp }
+        guard let dict = self.convertToDictionary(text: json) else { return resp }
+        guard let responseText = self.determineResponseText(dict:dict) else { return resp }
+        guard let httpCode = dict["code"] else { return resp }
+        guard let code =  httpCode as? Int32 else { return resp }
+        
+        return (code:code,response:responseText)
+    }
+    
+    /**
+     REMOVE STUBS
+     
+     - return: none
+     */
+    public func removeStubs(){
+        HTTPStubs.removeAllStubs()
+    }
+    
+    /**
      IS TEST
+     
+     - return: Bool
+     
      */
     public func isTest()->Bool{
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName), create: false)
-        if paste?.string != nil {
-            
-            return true
-        }
-        return false
+        if readClipboard() == nil { return false }
+        return true
     }
     
-    
-    /*
+    /**
      HAS MODEL TEST
+     
+     - return: Bool
+     
      */
     public func isModelTest()->Bool{
         
-        let paste = UIPasteboard(name: UIPasteboardName(rawValue: self.enviornmentName+"-model"), create: false)
-        if paste?.string != nil {
-            
-            return true
-        }
-        return false
+        if readDatabaseData() == nil { return false }
+        return true
     }
 }
--- Example/BDTestsExample/Pods/BDTests/README.md
+++ Example/BDTestsExample/Pods/BDTests/README.md
@@ -20,6 +20,81 @@ it, simply add the following line to your Podfile:
 pod "BDTests"
 ```
 
+## What is this for?
+This is a framework that is intended to simplify stubbing of network requests when performing UITests for iOS projects written in Swift. The second thing BDTests takes care of is creating an interface to help set up your app for the UITest you are trying to perform. For example, if your app requires login or you need your app in a certain state. BDTests creates a way for you to easily set up your state for the system under test. 
+
+## Set Up
+In your AppDelegate you need to import BDtests and add the testEnv method to your AppDelegate.
+
+```ruby
+import BDTests
+```
+
+```swift
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        //CHECK AND SET UP TESTS 
+        _ = BDTestsEnv().testEnv()
+
+        return true
+}
+```
+
+## Network Tests 
+Create an instance of BDTests: 
+```ruby
+let test = BDTests(enviornment:nil)
+```
+If you pass nil in the initializer BDTests will use a default name (String) as your enviornment name. This is usually fine.
+next create a test: 
+
+```ruby
+test.createTest(jsonString:nil, jsonFile:"stub-file",httpCode:200)
+```
+
+createTest will either accept a JSON string or the name of a file that needs to live in the main bundle of your app. In either case this will be the JSON that is stubbed as a response from the call that your app will make to your server. Finally provide the HTTP Response code you desire to test.
+
+This test will intercept the call made by your app to a server and return the JSON payload you used when you created the test. `
+
+
+## How to stub multiple requests
+In cases where you want to do multiple requests simply create a 2nd test with a different enviornment name:
+
+```ruby
+//TEST 1
+let sut = BDTests(enviornment:nil)
+let test = sut.createTest(jsonString: "{\"key\":\"value\"}" , jsonFile: nil, httpCode: 400)
+
+//DO SOME UI TESTS
+
+//TEST ANOTHER STUBBED CALL
+sut.createTest(jsonString: "{\"key2\":\"value2\"}" , jsonFile: nil, httpCode: 200)
+```
+
+## Setting up your app's state for a test
+If you want to to some set up prior to running your test that is not related to networking, for example seeding your database with some specific data follow these steps:
+
+  1. In your apps code create an extension of BDTestsHelper and add a method to do whatever set up you need doen here.
+
+```ruby
+import BDTests 
+
+extension BDTestsHelper {
+
+	func setUpMethod(){
+
+		//DO SOME SET UP HERE
+
+	}
+}
+```
+
+2. In your test code call the seedDatabase method and pass the name of your method that you want to call as a String value. GOTCHA!! You must do this prior to XCUIApplication().launch() being called or it will be ignored.
+
+```ruby
+let seeded =  sut.seedDatabase(ref: "setUpMethod")
+```
+
 ## Author
 
 bytedissident, dbronston@me.com
